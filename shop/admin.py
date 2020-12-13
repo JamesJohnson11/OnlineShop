86764c4979874b63c20ed08bfc98da478322a337
Barry Johnson <bdotmgmt@gmail.com>
2020-12-13 14:10:01 -0500

    Created product list in view in views file

--- shop/admin.py
+++ shop/admin.py
@@ -12,4 +12,4 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ['name', 'slug', 'price', 'available', 'created', 'updated']
     list_filter = ['available', 'created', 'updated']
     list_editable = ['price', 'available']
-    prepopulated_fields = {'slug', ('name',)}
+    prepopulated_fields = {'slug': ('name',)}
